7ec7d019f6d7c0715b303ffa21cb398fcd202f70
jpriegue <jorge.rodriguez@docplanner.com>
2019-11-06 16:08:05 +0100

    Initial state

---
+++ Account.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Bank_kata
+{
+    public class Account
+    {
+
+        public void deposit(int amount) { }
+
+        public void withdrawal(int amount) { }
+
+        public void printStatement() { }
+
+    }
+}
