66d66f1546833b0080afb5d10b42e5d716d9e03f
jpriegue <jorge.rodriguez@docplanner.com>
2019-11-14 15:43:56 +0100

    Refactor structure

---
+++ Bank/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class Account
+    {
+
+        public void Deposit(int amount) { }
+
+        public void Withdrawal(int amount) { }
+
+        public void PrintStatement() { }
+
+    }
+}
